7d6d18b542db04f11c6c4e6f42391836c18511ab
YuichiNAGAO <azkgfa03nagao@gmail.com>
2023-05-15 16:41:11 +0900

    databaseとの接続設定の実装

---
+++ initializers/database.go
@@ -0,0 +1,20 @@
+package initializers
+
+import (
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	var err error
+	dsn := "user:mysql@(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalln("Error connecting database")
+		panic(err.Error())
+	}
+}
